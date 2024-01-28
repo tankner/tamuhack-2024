82bc30bf896e819cf4a823d64faa9633614d617a
Eddie Phillips <85954305+eddiep24@users.noreply.github.com>
2024-01-27 22:20:42 -0600

    Results page runs

---
+++ client/src/components/SearchResultItem.js
@@ -0,0 +1,55 @@
+import React from 'react';
+
+const GoogleSearchResultItem = ({ title, snippet, url }) => {
+  return (
+    <div style={styles.container}>
+      <a href={url} target="_blank" rel="noopener noreferrer" style={styles.title}>
+        {title}
+      </a>
+      <p style={styles.snippet}>{snippet}</p>
+      <a href={url} target="_blank" rel="noopener noreferrer" style={styles.url}>
+        {url}
+      </a>
+    </div>
+  );
+};
+
+const styles = {
+    container: {
+      margin: '8px 0',
+      padding: '10px',
+      border: '1px solid #ddd',
+      borderRadius: '8px',
+      boxShadow: '0 2px 4px rgba(0, 0, 0, 0.1)',
+      fontFamily: 'Roboto, sans-serif',
+    },
+    urlContainer: {
+      display: 'flex',
+      alignItems: 'center',
+    },
+    title: {
+      fontSize: '16px',
+      fontWeight: '500',
+      color: '#1565C0',
+      textDecoration: 'none',
+      marginBottom: '5px',
+      display: 'block',
+    },
+    snippet: {
+      fontSize: '14px',
+      marginBottom: '5px',
+    },
+    url: {
+      fontSize: '13px',
+      color: '#424242',
+      textDecoration: 'none',
+      display: 'block',
+    },
+    favicon: {
+      width: '16px',
+      height: '16px',
+      marginRight: '8px',
+    },
+};
+
+export default GoogleSearchResultItem;
---
+++ client/src/data/israelContent.js
@@ -0,0 +1,161 @@
+// who is the bad hamas or israel
+export const h11 = "Neither is to blame."
+export const c11 = [
+    {
+        "title": "Who are the bad guys, Israel or the Hamas? : r/AskReddit",
+        "snippet": "Who are the bad guys, Israel or the Hamas? Upvote 5. Downvote 64 ... WRONG AND ISRAEL RULES” or “NOOOOO YOUR WRONG AND HAMAS RULES”. I really ...",
+        "url": "https://www.reddit.com/r/AskReddit/comments/173jzmt/who_are_the_bad_guys_israel_or_the_hamas/"
+    },
+    {
+        "title": "Hamas-Israel conflict: Here's what you need to know about ...",
+        "snippet": "Oct 10, 2023 — The attack marked the deadliest assault the country has suffered in generations and led Israeli Prime Minister Benjamin Netanyahu to declare ...",
+        "url": "https://www.cnbc.com/2023/10/10/hamas-israel-conflict-heres-what-you-need-to-know-about-hamas.html"
+    },
+    {
+        "title": "The Israel-Hamas War Is Making Americans Question Their Relationships",
+        "snippet": "Oct 31, 2023 — The Israel-Hamas War Is Making Americans Question Their Relationships ... bad guy in the Middle East? To many, this dispute is more alarming ...",
+        "url": "https://time.com/6329962/israel-hamas-personal-relationships-strain-us/"
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+]
+
+export const h22 = "Hamas."
+export const c22 =  [
+     {
+        "title": "Gaza war: a better understanding of the violence on both ...",
+        "snippet": "Nov 30, 2023 — Does this not mean that Israel's crime ranks lower in the hierarchy of evil? This assessment is also too simplistic. When they go after Hamas ...",
+        "url": "https://theconversation.com/gaza-war-a-better-understanding-of-the-violence-on-both-sides-might-give-us-a-chance-at-a-solution-216855"
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+]
+
+
+export const h33 = "Israel."
+export const c33 = [
+    {
+        "title": "Gaza: Why the War Won’t End",
+        "snippet": "Nov 2, 2023 — It is necessary to consider how bad the situation in Gaza was before Hamas launched its attack and how much worse things have already gotten ...",
+        "url": "https://www.csis.org/analysis/gaza-why-war-wont-end"
+    },
+    {
+        "title": "who is the \"bad guy\"? israel or hamas? : r/geopolitics",
+        "snippet": "The \"bad guys\" are the ones whose actions directly caused this to happen: the current Israeli administration under Netanyahu and Hamas. Both ...",
+        "url": "https://www.reddit.com/r/geopolitics/comments/18ak4kk/who_is_the_bad_guy_israel_or_hamas/"
+    },
+    {
+        "title": "Column: Who's to blame for the Hamas attack on Israel ...",
+        "snippet": "Oct 9, 2023 — ... as though Israel somehow summoned this evil upon itself and therefore what we should talk about is what Israel did wrong.” Advertisement.",
+        "url": "https://www.latimes.com/opinion/story/2023-10-09/israel-hamas-attacks-failure-security-surveillance-blame"
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    },
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    }, 
+]
+
+export const e2 = [
+    {
+        "title": "Israel-Gaza war: Can Hamas actually be eliminated? ...",
+        "snippet": 'Dec 12, 2023 — Best Mortgages for Bad Credit · Best Hardship Loans · How to Boost Your ... "To \'eliminate\' or destroy Hamas, Israel will have to destroy the ...',
+        "url": "https://www.cnbc.com/2023/12/12/israel-gaza-war-can-hamas-actually-be-eliminated-experts-weigh-in.html"
+    }, 
+    {
+        "title": "Misinformation, Disinformation, and the Israel-Hamas War",
+        "snippet": "Oct 23, 2023 — Bad information of any kind, intentional or accidental, leads to a “polluted information environment” that can make it hard to tell what's real ...",
+        "url": "https://www.bu.edu/articles/2023/israel-hamas-war-misinformation/"
+    }, 
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    }, 
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    }, 
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    }, 
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    }, 
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    }, 
+    {
+        "title": "",
+        "snippet": "",
+        "url": ""
+    }, 
+]
