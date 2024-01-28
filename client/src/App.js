9eef055cf498c1af5652e79f2b655d34623e6c56
Thomas Ankner <tankner@tamu.edu>
2024-01-27 23:05:35 -0600

    ahhh

--- client/src/App.js
+++ client/src/App.js
@@ -3,6 +3,11 @@ import SearchPage from './components/SearchPage';
 
 function App() {
   return (
+    // <Router>
+    //   <Routes>
+    //     <Route exact path="/" element={<div className="tc ma0 pa4 min-vh-100"><SearchPage/></div>} />
+    //   </Routes>
+    // </Router>
     <div className="tc ma0 pa4 min-vh-100">
       <SearchPage/>
     </div>
