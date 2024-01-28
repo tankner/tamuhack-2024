4072322bbc2121b88a9d700e023757b02ee79a93
Thomas Ankner <tankner@tamu.edu>
2024-01-28 10:25:39 -0600

    added some credibility scores

--- client/src/data/earthFlatContent.js
+++ client/src/data/earthFlatContent.js
@@ -4,42 +4,49 @@ export const c1 = [
         "title": "How Do We Know the Earth Is Round?",
         "snippet": "By watching as the Earth also rotates from that view, you can see that the Earth is a rotating sphere and is not flat. Image by NASA. Like the other planets, ...",
         "url": "https://askanearthspacescientist.asu.edu/top-question/flat-earth",
-        "credibility": "94"
+        "credibility": "92"
     },
     {
         "title": "Flat Earthers: What They Believe and Why",
         "snippet": "Mar 27, 2020 — Michael Marshall, project director of the Good Thinking Society in the U.K., talks about flat earth belief and its relationship to ...",
-        "url": "https://www.scientificamerican.com/podcast/episode/flat-earthers-what-they-believe-and-why/"
+        "url": "https://www.scientificamerican.com/podcast/episode/flat-earthers-what-they-believe-and-why/",
+        "credibility": "88"
     },
     {
         "title": "Fighting flat-Earth theory",
         "snippet": "Jul 14, 2020 — The rapper, a vocal proponent of “flat-Earth theory”, wanted to seek evidence that our planet is a disc, not a globe. His aim was to raise ...",
-        "url": "https://physicsworld.com/a/fighting-flat-earth-theory/"
+        "url": "https://physicsworld.com/a/fighting-flat-earth-theory/",
+        "credibility": "67"
     },
     {
         "title": "Is the Earth round?",
         "snippet": "Oct 4, 2023 — While the Earth appears to be round when viewed from the vantage point of space, it is actually closer to an ellipsoid.",
-        "url": "https://oceanservice.noaa.gov/facts/earth-round.html"
+        "url": "https://oceanservice.noaa.gov/facts/earth-round.html",
+        "credibility": "91"
     },
     {
         "title": "Why some people believe the Earth is flat",
         "snippet": "The question of whether the earth is flat has been debated throughout human history. The shape of Earth explains seasons, changes in weather, how the sun ...",
-        "url": "https://www.bbcearth.com/news/why-some-people-believe-the-earth-is-flat"
+        "url": "https://www.bbcearth.com/news/why-some-people-believe-the-earth-is-flat",
+        "credibility": "79"
     },
     {
         "title": "Podcast: Is the Earth flat, or a circle?",
         "snippet": "Oct 25, 2023 — Well, first things first a circle is a two-dimensional shape, so a circle is definitely flat. Listen to the mini-podcast below to hear more ...",
-        "url": "https://www.ucl.ac.uk/culture-online/case-studies/2023/oct/podcast-earth-flat-or-circle"
+        "url": "https://www.ucl.ac.uk/culture-online/case-studies/2023/oct/podcast-earth-flat-or-circle",
+        "credibility": "59"
     },
     {
         "title": "Is the Earth Flat?",
         "snippet": "Many people will probably wonder why it is necessary to write an article defending a round earth. Or, more specifically, an earth that is spherical.",
-        "url": "https://answersingenesis.org/astronomy/earth/is-the-earth-flat/"
+        "url": "https://answersingenesis.org/astronomy/earth/is-the-earth-flat/",
+        "credibility": "72"
     },
     {
         "title": "A flat earth",
         "snippet": "How can we measure the stars if we can't reach out and touch them? · The Earth and Moon. · In the formative years of astronomy there was a burning desire to ...",
-        "url": "https://www.khanacademy.org/partner-content/nasa/measuringuniverse/measure-the-solarsystem/a/a-flat-earth"
+        "url": "https://www.khanacademy.org/partner-content/nasa/measuringuniverse/measure-the-solarsystem/a/a-flat-earth",
+        "credibility": "82"
     },
 ]
 
@@ -48,13 +55,21 @@ export const c2 = [
     {
         "title": "Truth unboxed: the Earth is flat! - The Strand",
         "snippet": "Mar 28, 2023 — Since time immemorial, people who have believed in the Earth being a flat disc, not a 'geoid,' have been ridiculed and called 'lunatics' for ...",
-        "url": "https://thestrand.ca/truth-unboxed-the-earth-is-flat/"
+        "url": "https://thestrand.ca/truth-unboxed-the-earth-is-flat/",
+        "credibility": "31"
     },
     {
         "title": "Why the Earth is actually 100% flat",
         "snippet": "Jan 26, 2016 — Dean Burnett: Rapper B.o.B. has been criticised for publicly insisting the Earth is flat. You may think his claims are ludicrous, ...",
-        "url": "https://www.theguardian.com/science/brain-flapping/2016/jan/26/earth-totally-flat-conspiracy-bob"
-    }
+        "url": "https://www.theguardian.com/science/brain-flapping/2016/jan/26/earth-totally-flat-conspiracy-bob",
+        "credibility": "62"
+    },
+    {
+        "title": "The Flat Earth Society",
+        "snippet": "Welcome to The Flat Earth Society, home of the flat earth forums and the largest library of Flat Earth Society journals, newsletters, interviews, and books.",
+        "url": "https://theflatearthsociety.org/home/",
+        "credibility": "21"
+    }, 
 ]
 
 export const h3 = "Other results"
@@ -62,36 +77,37 @@ export const e1 = [
     {
         "title": "The flat-Earth conspiracy is spreading around the globe. ...",
         "snippet": "Nov 18, 2019 — I don't want to be a flat Earther,” David Weiss says, his voice weary as he reflects on his personal awakening. “Would you wake up in the ...",
-        "url": "https://www.cnn.com/2019/11/16/us/flat-earth-conference-conspiracy-theories-scli-intl/index.html"
+        "url": "https://www.cnn.com/2019/11/16/us/flat-earth-conference-conspiracy-theories-scli-intl/index.html",
+        "credibility": "78"
     }, 
     {
         "title": "Conspiracy vs. Science: A Survey of U.S. Public Beliefs",
         "snippet": "Apr 25, 2022 — On a nationwide U.S. survey, around 10 percent of respondents agreed with conspiracy claims that the Earth is flat, NASA faked the Moon ...",
-        "url": "https://carsey.unh.edu/publication/conspiracy-vs-science-a-survey-of-us-public-beliefs"
+        "url": "https://carsey.unh.edu/publication/conspiracy-vs-science-a-survey-of-us-public-beliefs",
+        "credibility": "83"
     }, 
     {
         "title": "Flat Earthers Spend Rs 16 Lakh On Experiment That ...",
         "snippet": "Flat Earthers Spend Rs 16 Lakh On Experiment That Accidentally Proves 'Earth Is Round'| Shorts The flat-earth and round-earth debate is a ...",
-        "url": "https://www.youtube.com/watch?v=viZcVYmxWNI"
+        "url": "https://www.youtube.com/watch?v=viZcVYmxWNI",
+        "credibility": "65"
     }, 
     {
         "title": "Shaquille O'Neal says he was just joking with flat-Earth ...",
         "snippet": "Mar 23, 2017 — Shaquille O'Neal confirmed Thursday what many had suspected: The Earth is, in fact, round. Oh, wait, everyone (well, almost) knew that ...",
-        "url": "https://www.washingtonpost.com/news/early-lead/wp/2017/03/23/shaquille-oneal-says-he-was-just-joking-with-flat-earth-comments/"
+        "url": "https://www.washingtonpost.com/news/early-lead/wp/2017/03/23/shaquille-oneal-says-he-was-just-joking-with-flat-earth-comments/",
+        "credibility": "81"
     }, 
     {
         "title": "Flat Earth",
         "snippet": "Flat Earth. 5527 likes. Flat Earth is an Helsinki-based alternative rock band known for their dynamic sound and electrifying performances. With a...",
-        "url": "https://www.facebook.com/OfficialFlatEarth.band/"
+        "url": "https://www.facebook.com/OfficialFlatEarth.band/",
+        "credibility": "36"
     }, 
     {
         "title": "Flat Earth Theatre - Home",
         "snippet": "Erin Lerch on Pinch Point, Playwriting, and... Penguins? Playwright Erin Lerch took a break from exploring Antarctica and befriending penguins to talk about ...",
-        "url": "https://www.flatearththeatre.com/"
+        "url": "https://www.flatearththeatre.com/",
+        "credibility": "41"
     },     
-    {
-        "title": "The Flat Earth Society",
-        "snippet": "Welcome to The Flat Earth Society, home of the flat earth forums and the largest library of Flat Earth Society journals, newsletters, interviews, and books.",
-        "url": "https://theflatearthsociety.org/home/"
-    }, 
 ]
